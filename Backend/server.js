99ab94a9d7161f700da7364153296239bd041175
Manjunath063 <manjunathjangam198@gmail.com>
2025-07-11 12:21:06 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -138,5 +138,5 @@ app.delete('/api/leave-requests', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://3.85.61.23:${port}`);
+    console.log(`Server running at http://13.51.13.185:${port}`);
 });
